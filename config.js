911543a9634cec43825d0bcab197dc69c4365572
Konie Shadow <guofengzuo@hotmail.com>
2019-05-15 19:38:50 +0800

    modify template

--- config.js
+++ config.js
@@ -1,17 +1,13 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
-  primaryColor: '#3498db', // Required
+  title: '柯弥林', // Required
+  author: '康尼', // Required
+  description: 'Konie\'s Blog',
+  primaryColor: '#cdb16d', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
   },
   pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  siteUrl: 'https://www.comellia.com',
 };
---
+++ src/pages/first-page/index.md
@@ -0,0 +1,9 @@
+---
+title: First Page
+date: "2019-05-15"
+featuredImage: './featured.jpg'
+---
+
+## First Page
+
+May the force be with you!
