a16e6ca8b1ce41976e5bf47fa338f326f0d87449
Konie Shadow <guofengzuo@hotmail.com>
2019-07-16 16:49:43 +0800

    change google analytics config

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-110532223-1`,
       },
     },
     `gatsby-plugin-feed`,
